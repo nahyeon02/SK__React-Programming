aec3498d25471d89e24d0055463ac05f7b6e6dd2
지훈 <yamoog@naver.com>
2020-11-17 13:51:50 +0900

    [실습] 클래스 → 함수 컴포넌트로 변경 (useState() 훅 사용)

--- react-practice/src/components/accordion/AccordionList.js
+++ react-practice/src/components/accordion/AccordionList.js
@@ -1,58 +1,38 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
 import { array } from 'prop-types'
 import AccordionHandle from './AccordionHandle'
 import AccordionPanel from './AccordionPanel'
 
-export default class accordionList extends Component {
-  // 속성 검사
-  static propTypes = {
-    list: array.isRequired, // 필수 입력 속성
-  }
+/* -------------------------------------------------------------------------- */
 
-  // 상태
-  // 클래스 필드 문법 (ES 표준 제안 상태 → 표준으로 결정될 예정)
-  state = {
-    list: null,
-    isLoading: false,
-    hasError: null,
-    currentIndex: 0,
-    activeClassName: 'is--active',
-  }
+export default function AccordionList({ list, ...restProps }) {
+  // 함수 컴포넌트의 상태로 활용
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-  // 상태 변경 메서드(자식 컴포넌트 전달(props) ← 콜백(callback))
-  // 클래스 필드 문법을 사용해 화살표 함수 활용
-  changeCurrentIndex = (index) => {
-    // 상태 변경
-    this.setState({
-      currentIndex: index,
-    })
-  }
+  return (
+    <ul {...restProps} className="accordion">
+      {list.map((item, index) => (
+        <li key={item.id} className="accordion__item">
+          <AccordionHandle
+            className="accordion__handle"
+            content={item.handleText}
+            index={index}
+            active={currentIndex === index}
+            onChangeIndex={setCurrentIndex}
+          />
+          <AccordionPanel
+            className="accordion__panel"
+            active={currentIndex === index}
+          >
+            {item.panelInfo}
+          </AccordionPanel>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
-  // 렌더링
-  render() {
-    const { list } = this.props
-    const { currentIndex } = this.state
-
-    return (
-      <ul className="accordion">
-        {list.map((item, index) => (
-          <li key={item.id} className="accordion__item">
-            <AccordionHandle
-              className="accordion__handle"
-              content={item.handleText}
-              index={index}
-              active={currentIndex === index}
-              onChangeIndex={this.changeCurrentIndex}
-            />
-            <AccordionPanel
-              className="accordion__panel"
-              active={currentIndex === index}
-            >
-              {item.panelInfo}
-            </AccordionPanel>
-          </li>
-        ))}
-      </ul>
-    )
-  }
+// 함수형 컴포넌트의 속성 검사
+AccordionList.propTypes = {
+  list: array.isRequired,
 }
---
+++ react-practice/src/components/accordion/accordionList.Class.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { array } from 'prop-types'
+import AccordionHandle from './AccordionHandle'
+import AccordionPanel from './AccordionPanel'
+
+export default class accordionList extends Component {
+  // 속성 검사
+  static propTypes = {
+    list: array.isRequired, // 필수 입력 속성
+  }
+
+  // 상태
+  // 클래스 필드 문법 (ES 표준 제안 상태 → 표준으로 결정될 예정)
+  state = {
+    list: null,
+    isLoading: false,
+    hasError: null,
+    currentIndex: 0,
+    activeClassName: 'is--active',
+  }
+
+  // 상태 변경 메서드(자식 컴포넌트 전달(props) ← 콜백(callback))
+  // 클래스 필드 문법을 사용해 화살표 함수 활용
+  changeCurrentIndex = (index) => {
+    // 상태 변경
+    this.setState({
+      currentIndex: index,
+    })
+  }
+
+  // 렌더링
+  render() {
+    const { list } = this.props
+    const { currentIndex } = this.state
+
+    return (
+      <ul className="accordion">
+        {list.map((item, index) => (
+          <li key={item.id} className="accordion__item">
+            <AccordionHandle
+              className="accordion__handle"
+              content={item.handleText}
+              index={index}
+              active={currentIndex === index}
+              onChangeIndex={this.changeCurrentIndex}
+            />
+            <AccordionPanel
+              className="accordion__panel"
+              active={currentIndex === index}
+            >
+              {item.panelInfo}
+            </AccordionPanel>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
