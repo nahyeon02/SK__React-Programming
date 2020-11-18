e4f6c57efb7ef65538b9f0ce94de000debe592cc
지훈 <yamoog@naver.com>
2020-11-18 09:13:48 +0900

    [학습 자료 정리] remote react-d3-practice

--- react-d3-practice/src/components/accordion/index.js
+++
@@ -1,5 +0,0 @@
-// ES import, export
-// 다시 내보내기 (re-exporting)
-export { default as AccordionList } from './AccordionList'
-export { default as AccordionHandle } from './AccordionHandle'
-export { default as AccordionPanel } from './AccordionPanel'
